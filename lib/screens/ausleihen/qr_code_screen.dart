b0a72d6431b6522f84d50b8aeaa391ad683c139e
drmilde <milde@hs-fulda.de>
2021-06-17 20:16:44 +0200

    QrScreen ... GestureDetector für einfachere Navigation

--- lib/screens/ausleihen/qr_code_screen.dart
+++ lib/screens/ausleihen/qr_code_screen.dart
@@ -31,58 +31,63 @@ class QrCodeScreen extends StatelessWidget {
           title: Text(config.getAppbarTitle()),
           backgroundColor: config.getPrimaryColor(),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 2,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Übertragene Daten im QRCode: ",
-                  style: GoogleFonts.nunito(
-                    fontSize: 14,
+        body: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 2,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Übertragene Daten im QRCode: ",
+                    style: GoogleFonts.nunito(
+                      fontSize: 14,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Container(
-                    width: double.infinity,
-                    color: Theme.of(context).bottomAppBarColor,
-                    child: Text(
-                      data,
-                      style: TextStyle(
-                        fontFamily: "Courier",
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Center(
                     child: Container(
-                      height: 300,
-                      child: QrImage(
-                        data: data,
-                        version: QrVersions.auto,
-                        //size: MediaQuery.of(context).size.width * 0.5,
+                      width: double.infinity,
+                      color: Theme.of(context).bottomAppBarColor,
+                      child: Text(
+                        data,
+                        style: TextStyle(
+                          fontFamily: "Courier",
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 8,
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Center(
+                      child: Container(
+                        height: 300,
+                        child: QrImage(
+                          data: data,
+                          version: QrVersions.auto,
+                          //size: MediaQuery.of(context).size.width * 0.5,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ));
   }
